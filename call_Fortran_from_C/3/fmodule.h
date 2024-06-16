9ed1a481a37a2f4e4ccedddfafaa25d0a2972217
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-06-16 12:32:55 +0200

    add case 3

---
+++ call_Fortran_from_C/3/fmodule.h
@@ -0,0 +1,10 @@
+#ifndef FMODULE_H
+#define FMODULE_H
+
+// Typedef for the function pointer
+typedef float (*func_ptr)(float *);
+
+// Function prototype for the Fortran wrapper subroutine
+void integrate_c(func_ptr f, float *a, float *b, int *n, float *result);
+
+#endif // FMODULE_H
