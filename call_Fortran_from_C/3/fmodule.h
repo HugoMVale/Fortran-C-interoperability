26c8be78c7f845bfa37e592825171b962c8a4389
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-06-16 17:12:36 +0200

    add missing extern

--- call_Fortran_from_C/3/fmodule.h
+++ call_Fortran_from_C/3/fmodule.h
@@ -5,6 +5,6 @@
 typedef float (*func_ptr)(float *);
 
 // Function prototype for the Fortran wrapper subroutine
-void integrate_c(func_ptr f, float *a, float *b, int *n, float *result);
+extern void integrate_c(func_ptr f, float *a, float *b, int *n, float *result);
 
 #endif // FMODULE_H
