556ba033a9aae600cc9f82d2cb174bf126e75ab2
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-06-23 12:33:53 +0200

    fprettify

--- call_Fortran_from_C/3/fprogram.f90
+++ call_Fortran_from_C/3/fprogram.f90
@@ -2,7 +2,7 @@ program fprogram
    use fmodule, only: open_file
    implicit none
 
-   character(len=*), parameter :: filename="hello_fortran.txt"
+   character(len=*), parameter :: filename = "hello_fortran.txt"
 
    call open_file(8, filename)
 
